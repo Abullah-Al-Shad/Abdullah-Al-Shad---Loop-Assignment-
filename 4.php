4a45a10a1eaf60e8a9e10d2573582f74ac3c740b
Shad <abdullahshad85@gmail.com>
2021-12-02 11:13:18 +0600

    Loop Assignment

---
+++ 4.php
@@ -0,0 +1,61 @@
+<?php
+
+
+// Multiplication Table of- 2
+
+
+$x = 2;
+
+for ($y = 1; $y <= $x*10; $y++){
+    if($y % $x == 0){
+        $z = $y/$x;
+        echo "$x*$z=$y <br>";
+    
+    }
+}
+echo "<hr>";
+
+
+// Multiplication Table of- 3
+
+
+$x = 3;
+
+for ($y = 1; $y <= $x*10; $y++){
+    if($y % $x == 0){
+        $z = $y/$x;
+        echo "$x*$z=$y <br>";
+    
+    }
+}
+echo "<hr>";
+
+
+// Multiplication Table of- 4
+
+
+$x = 4;
+
+for ($y = 1; $y <= $x*10; $y++){
+    if($y % $x == 0){
+        $z = $y/$x;
+        echo "$x*$z=$y <br>";
+    
+    }
+}
+echo "<hr>";
+
+
+// Multiplication Table of- 5
+
+
+$x = 5;
+
+for ($y = 1; $y <= $x*10; $y++){
+    if($y % $x == 0){
+        $z = $y/$x;
+        echo "$x*$z=$y <br>";
+    
+    }
+}
+echo "<hr>";
